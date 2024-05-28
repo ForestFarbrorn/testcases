11377bd8888be1f7562d29479c28df3f9799610c
Oskar Sundell <sundell.oskar@gmail.com>
2024-05-28 20:52:35 +0200

    Mood

--- testfall.py
+++ testfall.py
@@ -1,5 +1,5 @@
 from unittest.mock import MagicMock
-from fall import SmartHome, mood_profile
+from fall import SmartHome
 
 def test_light_switch(mocker):
         
@@ -16,8 +16,24 @@ def test_light_switch(mocker):
 def test_mood_profile(mocker):
     
     mock_mood_profile = mocker.patch.object(SmartHome, 'mood_profile')
-    set_mood_profile = mood_profile 
+    get_mood_profile = new_mood_profile(mood = 'Funky')
 
-    assert isinstance()
+    mock_instance.assert_called_once()
+    assert mood == 'Funky'
 
 def test_scheduling(mocker):
+      
+def test_get_all_questions_returns_list(mocker):
+    mock_db_instance = mocker.patch.object(
+        QuestionDB, 
+        'get_all_questions', 
+        return_value=[Question(qid=0, question_text='Test question', answer_text='Test answer')]
+    )
+
+    questions = get_all_questions()
+
+    assert isinstance(questions, list)
+    assert len(questions) == 1
+    assert questions[0].question_text == 'Test question'
+    assert questions[0].answer_text == 'Test answer'
+    mock_db_instance.assert_called_once()
