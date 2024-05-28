4b2d3e850387ef2aaadae102909e604b65f4f1f8
Oskar Sundell <sundell.oskar@gmail.com>
2024-05-28 19:13:33 +0200

    New

--- fall.py
+++ fall.py
@@ -1,2 +1,17 @@
-# Chokladboll
 from pydantic import BaseModel
+
+# class for Smart home
+class SmartHome():
+    def __init__(self, state: int, room: str, floor: int) -> None:
+        self.state = state
+        self.room = room
+        self.floor = floor
+    
+    def light_switch():
+
+
+
+def Mood():
+
+
+def Scheduling():
